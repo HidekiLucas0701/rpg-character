bfa5edd036ab379c67390cc2e36f5c0794ef6fc2
Lucas Hideki Penha Suguiura de Souza <hideki.lucas0701@gmail.com>
2026-03-31 20:08:49 -0300

    feat(wip): request dto with validations and conversion method

---
+++ src/main/java/fiap/com/br/rpgcharacter/dto/CharacterRequest.java
@@ -0,0 +1,58 @@
+package fiap.com.br.rpgcharacter.dto;
+
+import fiap.com.br.rpgcharacter.entity.Character;
+import fiap.com.br.rpgcharacter.enums.CharacterClass;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+
+
+public record CharacterRequest (
+
+        @NotBlank(message = "Name is required")
+        @Min(3)
+        @Max(50)
+        String name,
+
+        @NotBlank(message = "Email is required")
+        @Email
+        String email,
+
+        @NotBlank(message = "Age is required")
+        @Min(12)
+        @Max(120)
+        Integer age,
+
+        @NotBlank(message = "Class is required")
+        CharacterClass characterClass,
+
+        @NotBlank(message = "Level is required")
+        @Min(1)
+        @Max(100)
+        Integer level,
+
+        @NotBlank(message = "Hp is required")
+        @Min(0)
+        Double hp,
+
+        @NotBlank(message = "Creation date is required")
+        @Past
+        LocalDate createdAt,
+
+        @NotBlank(message = "Code is required")
+        @Pattern(regexp = "^[A-Z]{4}-\\d{4}$", message = "ABCD-1234")
+        String characterCode
+) {
+        public Character toEntity(){
+            return Character.builder()
+                    .name(name)
+                    .email(email)
+                    .age(age)
+                    .characterClass(characterClass)
+                    .level(level)
+                    .hp(hp)
+                    .createdAt(createdAt)
+                    .characterCode(characterCode)
+                    .build();
+        }
+}
