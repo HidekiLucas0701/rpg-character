4e1300d05fe6a358a13e8b4e136874ec87b5f0fd
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-31 19:16:16 -0300

    Initial Commit

---
+++ src/main/java/fiap/com/br/rpgcharacter/RpgcharacterApplication.java
@@ -0,0 +1,13 @@
+package fiap.com.br.rpgcharacter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgcharacterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgcharacterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fiap/com/br/rpgcharacter/RpgcharacterApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.com.br.rpgcharacter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgcharacterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
