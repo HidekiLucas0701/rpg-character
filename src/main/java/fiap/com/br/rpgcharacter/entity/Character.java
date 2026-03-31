10cde236f8d863f4415bddeebba0e13f18c93cf0
Lucas Hideki Penha Suguiura de Souza <hideki.lucas0701@gmail.com>
2026-03-31 20:11:56 -0300

    feat(wip): builder design pattern annotation

--- src/main/java/fiap/com/br/rpgcharacter/entity/Character.java
+++ src/main/java/fiap/com/br/rpgcharacter/entity/Character.java
@@ -5,12 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Entity
 @Data
+@Builder
 public class Character {
 
     @Id
